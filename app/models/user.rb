2a59f684695a66a2edcdc5e52845d502e5c25c5a
MacInnes <califormula@gmail.com>
2018-07-18 13:53:19 -0600

    slight refactor of users and messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   validates_presence_of :password, require: true
   validates_presence_of :email, require: true
 
-  has_many :messages_from, :class_name => "Messages", :foreign_key => "user_from_id"
-  has_many :messages_to, :class_name => "Messages", :foreign_key => "user_to_id"
+  has_many :messages_from, :class_name => "Message", :foreign_key => "user_from_id"
+  has_many :messages_to, :class_name => "Message", :foreign_key => "user_to_id"
   has_many :stickers
   has_secure_password
 
